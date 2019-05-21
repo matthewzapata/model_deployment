c3c3da4254d053ef82fd8974d5f9446d79759b3b
Matthew Zapata <matthew.zapata21@gmail.com>
2019-05-21 11:29:41 -0500

    Add rool dice to server.py

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world!'
+
+@app.route('/roll-dice')
+def roll():
+    number = np.random.randint(1, 7)
+    return f'You rolled a {number}'
